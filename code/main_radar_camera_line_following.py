9ececf5cc5d93e6492f1cb2eae1f503ab8796361
Cooper <crj31415926@gmail.com>
2026-07-31 12:26:46 +0800

    Reduce car trace sampling rate

--- code/main_radar_camera_line_following.py
+++ code/main_radar_camera_line_following.py
@@ -467,10 +467,10 @@ class RadarCameraLineApplication:
                 on_start_mission=self._fleet_start_mission,
                 trace_options=TraceSamplingOptions(
                     enabled=True,
-                    sample_interval_s=0.10,
+                    sample_interval_s=0.50,
                     buffer_capacity=600,
-                    min_distance_cm=1.0,
-                    stationary_keepalive_s=1.0,
+                    min_distance_cm=5.0,
+                    stationary_keepalive_s=2.0,
                 ),
             )
 
